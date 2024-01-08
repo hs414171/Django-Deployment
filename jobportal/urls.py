1a8d27e064fe27d619f2d6bfeab4881b0854ddb7
Harshit Sharma <hs414171@gmail.com>
2024-01-08 12:59:40 +0530

    Vercel work

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    path('',views.Index),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+def Index(request):
+    return HttpResponse('<h1>welcome to the app</h1>')
 
-# Create your views here.
